1aed96c5574dd6f856a69601798164ba84af56da
ytaka0407 <spicayt@gmail.com>
2020-12-03 18:53:03 +0900

    チャットルーム参加状況テーブル作成のDDL文記述

--- db/db-challenge2/チャットルームDDL文.sql
+++ db/db-challenge2/チャットルームDDL文.sql
@@ -3,7 +3,7 @@ CREATE TABLE members(
     mail_address varchar(100) NOT NULL,
     nickname varchar(100) NOT NULL,
     profile varchar(1000),
-    business_phone_num  varchar(13),
+    business_phone_num varchar(13),
     mobile_phone_num varchar(13),
     login_password varchar(100) NOT NULL,
     is_deleted tinyint(1) DEFAULT 0 NOT NULL,
@@ -16,9 +16,14 @@ CREATE TABLE chatrooms(
     room_description varchar(1000),
     file_transmission tinyint(1) DEFAULT 1 NOT NULL,
     chat_type tinyint(1) DEFAULT 0 NOT NULL,
-    is_deleted  tinyint(1) DEFAULT 0 NOT NULL,
+    is_deleted tinyint(1) DEFAULT 0 NOT NULL,
     create_member_id integer(10) NOT NULL REFERENCES members(user_id),
     update_member_id integer(10) NOT NULL REFERENCES members(user_id),
-    created_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+    created_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP
-)
+);
+CREATE TABLE chatroom_joined_list(
+    member_id integer(10) NOT NULL REFERENCES members(user_id),
+    chatroom_id integer(10) NOT NULL REFERENCES members(user_id),
+    joined_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
