5268e790c4b19fa7005e7cbf440d2bb5a0eb45ca
ytaka0407 <spicayt@gmail.com>
2020-12-05 16:43:54 +0900

    大文字小文字の記述方法を統一

--- db/db-challenge2/チャットルームDDL文.sql
+++ db/db-challenge2/チャットルームDDL文.sql
@@ -1,54 +1,54 @@
 CREATE TABLE members(
-    user_id integer(10) PRIMARY KEY AUTO_INCREMENT,
-    mail_address varchar(100) NOT NULL,
-    nickname varchar(100) NOT NULL,
-    profile varchar(1000),
-    business_phone_num varchar(13),
-    mobile_phone_num varchar(13),
-    login_password varchar(100) NOT NULL,
-    is_deleted tinyint(1) DEFAULT 0 NOT NULL,
-    joined_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP
+    user_id INTEGER(10) PRIMARY KEY AUTO_INCREMENT,
+    mail_address VARCHAR(100) NOT NULL,
+    nickname VARCHAR(100) NOT NULL,
+    profile VARCHAR(1000),
+    business_phone_num VARCHAR(13),
+    mobile_phone_num VARCHAR(13),
+    login_password VARCHAR(100) NOT NULL,
+    is_deleted TINYINT(1) DEFAULT 0 NOT NULL,
+    joined_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP
 );
 CREATE TABLE chatrooms(
-    room_id integer(10) PRIMARY KEY AUTO_INCREMENT,
-    room_name varchar(100) NOT NULL,
-    room_description varchar(1000),
-    file_transmission tinyint(1) DEFAULT 0 NOT NULL,
-    chat_type tinyint(1) DEFAULT 0 NOT NULL,
-    is_deleted tinyint(1) DEFAULT 0 NOT NULL,
-    create_member_id integer(10) NOT NULL REFERENCES members(user_id),
-    update_member_id integer(10) NOT NULL REFERENCES members(user_id),
-    created_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP
+    room_id INTEGER(10) PRIMARY KEY AUTO_INCREMENT,
+    room_name VARCHAR(100) NOT NULL,
+    room_description VARCHAR(1000),
+    file_transmission TINYINT(1) DEFAULT 0 NOT NULL,
+    chat_type TINYINT(1) DEFAULT 0 NOT NULL,
+    is_deleted TINYINT(1) DEFAULT 0 NOT NULL,
+    create_member_id INTEGER(10) NOT NULL REFERENCES members(user_id),
+    update_member_id INTEGER(10) NOT NULL REFERENCES members(user_id),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP
 );
 CREATE TABLE chatroom_joined_list(
-    member_id integer(10) NOT NULL REFERENCES members(user_id),
-    chatroom_id integer(10) NOT NULL REFERENCES members(user_id),
-    joined_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    member_id INTEGER(10) NOT NULL REFERENCES members(user_id),
+    chatroom_id INTEGER(10) NOT NULL REFERENCES members(user_id),
+    joined_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY(member_id, chatroom_id)
 );
 CREATE TABLE posts(
-    post_id integer(10) PRIMARY KEY AUTO_INCREMENT,
-    chatroom_id integer(10) NOT NULL REFERENCES chatrooms(room_id),
-    post_text varchar(1000) NOT NULL,
-    post_filename varchar(10),
-    is_deleted tinyint(1) DEFAULT 0 NOT NULL,
-    post_member_id integer(10) NOT NULL REFERENCES members(user_id),
-    update_member_id integer(10) NOT NULL REFERENCES members(user_id),
-    created_at datetime DEFAULT current_timestamp NOT NULL,
-    updated_at datetime DEFAULT current_timestamp NOT NULL ON update current_timestamp
+    post_id INTEGER(10) PRIMARY KEY AUTO_INCREMENT,
+    chatroom_id INTEGER(10) NOT NULL REFERENCES chatrooms(room_id),
+    post_text VARCHAR(1000) NOT NULL,
+    post_filename VARCHAR(10),
+    is_deleted TINYINT(1) DEFAULT 0 NOT NULL,
+    post_member_id INTEGER(10) NOT NULL REFERENCES members(user_id),
+    update_member_id INTEGER(10) NOT NULL REFERENCES members(user_id),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL ON update CURRENT_TIMESTAMP
 );
 CREATE TABLE tasks(
-    task_id integer(10) PRIMARY KEY AUTO_INCREMENT,
-    task_description varchar(1000) NOT NULL,
-    assigned_member_id integer(10) NOT NULL REFERENCES members(user_id),
-    status tinyint(1) DEFAULT 0 NOT NULL,
-    due_date datetime,
-    rerated_chatroom_id integer(10) NOT NULL REFERENCES chatrooms(room_id),
-    is_deleted tinyint(1) DEFAULT 0 NOT NULL,
-    create_member_id integer(10) REFERENCES members(user_id),
-    update_member_id integer(10) REFERENCES members(user_id),
-    created_at datetime DEFAULT CURRENT_TIMESTAMP,
-    updated_at datetime DEFAULT CURRENT_TIMESTAMP
+    task_id INTEGER(10) PRIMARY KEY AUTO_INCREMENT,
+    task_description VARCHAR(1000) NOT NULL,
+    assigned_member_id INTEGER(10) NOT NULL REFERENCES members(user_id),
+    status TINYINT(1) DEFAULT 0 NOT NULL,
+    due_date DATETIME,
+    rerated_chatroom_id INTEGER(10) NOT NULL REFERENCES chatrooms(room_id),
+    is_deleted TINYINT(1) DEFAULT 0 NOT NULL,
+    create_member_id INTEGER(10) REFERENCES members(user_id),
+    update_member_id INTEGER(10) REFERENCES members(user_id),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
 )
