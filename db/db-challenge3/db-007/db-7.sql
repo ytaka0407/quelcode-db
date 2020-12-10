1dcdedf1734a6212484ef12c02e43ed8b91204f4
ytaka0407 <spicayt@gmail.com>
2020-12-11 01:40:18 +0900

    db‐7を作成・実行

---
+++ db/db-challenge3/db-007/db-7.sql
@@ -0,0 +1,21 @@
+SELECT
+    id,
+    room_name,
+    CASE
+        is_file_transmitable
+        WHEN 1 then '許可'
+        ELSE '禁止'
+    END AS is_file_transmitable,
+    is_one_on_one,
+    room_description,
+    is_deleted,
+    create_member_id,
+    update_member_id,
+    created_at,
+    updated_at
+FROM
+    chat_rooms
+WHERE
+    is_deleted = 0
+ORDER BY
+    id asc;
