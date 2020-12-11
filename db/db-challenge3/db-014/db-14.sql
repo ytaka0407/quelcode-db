43ec513b2b0f0ce951ef4e0a7096681bb1dc4207
ytaka0407 <spicayt@gmail.com>
2020-12-12 00:37:20 +0900

    要件を見落としていたため再度作成・実行。

--- db/db-challenge3/db-014/db-14.sql
+++ db/db-challenge3/db-014/db-14.sql
@@ -3,6 +3,16 @@ BEGIN;
 SET
     TRANSACTION ISOLATION LEVEL READ COMITTED;
 
-DROP TABLE chat_rooms_joined_list;
+DELETE FROM
+    chat_rooms_joined_list
+WHERE
+    member_id IN (
+        SELECT
+            id
+        FROM
+            members
+        WHERE
+            is_deleted = 1
+    );
 
 COMMIT;
